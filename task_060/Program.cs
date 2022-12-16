2fd13ace4be9874eabd57659f46e494e9e69581e
Haman692 <haman692@yandex.ru>
2022-12-16 16:13:53 +0700

    task060

---
+++ task_060/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+void PrintCube(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                System.Console.Write($"{arr[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+
+void FillCubeRandomNumbers(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+}
+
+int AddNumber(string meseg)
+{
+    System.Console.WriteLine(meseg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int cubeSize = AddNumber("Введите размер трехмерного масива");
+//int[,,] cube = new int[cubeSize, cubeSize, cubeSize];
+// FillCubeRandomNumbers(cube);
+int[,,] cube = {{{13, 12}, {43, 22}, {23, 66}}};
+PrintCube(cube);
